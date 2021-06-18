92f45334683f2e15664fd08ef98cbed94b64f7e2
theDreamer911 <thetrulydreamer@gmail.com>
2021-06-19 03:05:56 +0700

    Day 21 - Learn and Build Model for Classify Orange Ice or Ice Tea

---
+++ day21/data/image_downloader.py
@@ -0,0 +1,66 @@
+# First Section: Importing Libraries
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# Second Section: Declare important variables
+google_image = "https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&"
+
+user_agent = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+}
+
+# Third Section: Build the main function
+saved_folder = 'images'
+
+
+def main():
+    if not os.path.exists(saved_folder):
+        os.mkdir(saved_folder)
+    download_images()
+
+
+# Fourth Section: Build the download function
+def download_images():
+    data = input('What are you looking for? ')
+    n_images = int(input('How many images do you want? '))
+
+    print('searching...')
+
+    search_url = google_image + 'q=' + data
+
+    response = requests.get(search_url, headers=user_agent)
+
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    results = soup.findAll('img', {'class': 'rg_i Q4LuWd'})
+
+    count = 1
+    links = []
+    for result in results:
+        try:
+            link = result['data-src']
+            links.append(link)
+            count += 1
+            if(count > n_images):
+                break
+
+        except KeyError:
+            continue
+
+    print(f"Downloading {len(links)} images...")
+
+    for i, link in enumerate(links):
+        response = requests.get(link)
+
+        image_name = saved_folder + '/' + data + str(i+1) + '.jpg'
+
+        with open(image_name, 'wb') as fh:
+            fh.write(response.content)
+
+
+# Fifth Section: Run your code
+if __name__ == "__main__":
+    main()
---
+++ day21/note.md
@@ -0,0 +1,5 @@
+If you want to know about how to use the script for downloading the bulk of images
+
+Follow this tutorial
+
+[LINKS](https://python.plainenglish.io/how-to-automatically-download-bulk-images-for-your-dataset-using-python-f1efffba7a03)
