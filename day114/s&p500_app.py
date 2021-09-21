54bb98d02194df2cfef9724e60b72ed050c4a8d0
theDreamer911 <thetrulydreamer@gmail.com>
2021-09-21 19:34:49 +0700

    day114 - eda s&p500 stock

---
+++ day114/s&p500_app.py
@@ -0,0 +1,84 @@
+from altair.vegalite.v4.schema.core import FontWeight
+import matplotlib.pyplot as plt
+import streamlit as st
+import yfinance as yf
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import base64
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title("S&P 500 App")
+
+st.markdown("""
+This app would retrieve data from Wikipedia and it's coresponding with **Stock Closing Price**
+""")
+
+st.sidebar.header("User Input Features")
+
+
+@st.cache
+def load_data():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    return df
+
+
+df = load_data()
+sector = df.groupby("GICS Sector")
+
+# Sidebar - Sector selection
+sorted_sector_unique = sorted(df['GICS Sector'].unique())
+selected_sector = st.sidebar.multiselect(
+    "Sector", sorted_sector_unique, sorted_sector_unique)
+
+# Filtering data
+df_selected_sector = df[(df["GICS Sector"].isin(selected_sector))]
+
+st.header("Display Companies in Selected Sector")
+st.write("Data Dimension: " + str(df_selected_sector.shape[0]) + " rows and " + str(
+    df_selected_sector.shape[1]) + ' columns.')
+st.dataframe(df_selected_sector)
+
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    return href
+
+
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+
+data = yf.download(
+    tickers=list(df_selected_sector[:10].Symbol),
+    period="ytd",
+    interval="1d",
+    group_by="ticker",
+    auto_adjust=True,
+    prepost=True,
+    threads=True,
+    proxy=None
+)
+
+
+def price_plot(symbol):
+    df = pd.DataFrame(data[symbol].Close)
+    df['Date'] = df.index
+    plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+    plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+    plt.xticks(rotation=90)
+    plt.title(symbol, fontweight='bold')
+    plt.xlabel("Date", fontweight='bold')
+    plt.ylabel("Closing Price", fontweight='bold')
+    return st.pyplot()
+
+
+num_company = st.sidebar.slider("Number of Companies", 1, 5)
+
+if st.button("Show Plots"):
+    st.header("Stock Closing Price")
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
